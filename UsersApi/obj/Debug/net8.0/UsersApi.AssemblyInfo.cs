16157ba6fdaf7ac933e3460cfab6eff28e94b1d5
Gregory <gregory_gr@outlook.com>
2025-02-27 13:45:51 -0300

    first commit

--- UsersApi/obj/Debug/net8.0/UsersApi.AssemblyInfo.cs
+++ UsersApi/obj/Debug/net8.0/UsersApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d281cfad4e8eca31ff43614632eecc7223a252b")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
