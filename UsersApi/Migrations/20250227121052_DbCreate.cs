6d281cfad4e8eca31ff43614632eecc7223a252b
Gregory <gregory_gr@outlook.com>
2025-02-27 12:54:48 -0300

    first commit

---
+++ UsersApi/Migrations/20250227121052_DbCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UsersApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Login = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Funcao = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ UsersApi/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BCrypt.Net;
+
+namespace UsersApi.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "O campo Nome deve ter entre 3 e 200 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Login é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo Login deve ser um e-mail válido")]
+        [StringLength(100, ErrorMessage = "O campo Login pode ter no máximo 100 caracteres")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O campo Senha é obrigatório")]
+        [StringLength(200, ErrorMessage = "A senha deve ter entre 6 e 200 caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "O campo Função é obrigatório")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "O campo Função deve ter entre 3 e 20 caracteres")]
+        public string Funcao { get; set; }
+
+        public void SetPassword(string passwordHash)
+        {
+            Password = BCrypt.Net.BCrypt.HashPassword(passwordHash);
+        }
+
+        public bool VerifyPassword(string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(passwordHash, Password);
+        }
+    }
+}
